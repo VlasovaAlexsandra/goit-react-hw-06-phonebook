283a3fa493a6b73a0a7f3ec7dbf637949f658d02
VlasovaAlexsandra <68018046+VlasovaAlexsandra@users.noreply.github.com>
2021-04-21 01:33:26 +0300

    add kit

--- src/redux/Contacts/store.js
+++ src/redux/Contacts/store.js
@@ -1,11 +1,15 @@
-import { createStore, combineReducers } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import contactsReducer from './contacts-reducer'
 
 const rootReducer = combineReducers({
     contacts: contactsReducer
 })
 
-const store = createStore(rootReducer, composeWithDevTools())
+// const store = createStore(rootReducer, composeWithDevTools())
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: process.env.NODE_ENV === 'development'
+})
 
 export default store
